3c5dfc9d0e79562a8457c4e43d05eb0c55da1313
JayAhir17 <158028334+JayAhir17@users.noreply.github.com>
2024-08-05 12:13:48 +0530

    Hook_Assignment

---
+++ src/Login.jsx
@@ -0,0 +1,12 @@
+// rfce
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <h3 className='loginTxt'> Thank you ,Login Successful</h3>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/Logout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logout = () => {
+    return (
+        <div>
+            <h3 className='logoutTxt'> Visit again , Logout Successful</h3>
+        </div>
+    )
+}
+
+export default Logout
